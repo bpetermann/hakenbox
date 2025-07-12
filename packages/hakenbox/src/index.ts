389ca19b7cbc7158016119c49f246410e0cf7a92
bpetermann <benjamin.petermann@gmx.at>
2025-07-12 16:15:09 +0200

    feat: add use local storage csr

--- packages/hakenbox/src/index.ts
+++ packages/hakenbox/src/index.ts
@@ -1,3 +1,4 @@
 export { useDebounce } from './lib/hooks/useDebounce';
+export { useLocalStorageCSR } from './lib/hooks/useLocalStorageCSR';
 export { useLocalStorageState } from './lib/hooks/useLocalStorageState';
 export { useParams } from './lib/hooks/useParams';
